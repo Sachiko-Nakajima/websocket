68c4c45420c922ac7d8dcce7e9f06f26a8269024
Sachiko Nakajima <sachikonakajima@Sachikos-MacBook-Pro-2.local>
2020-06-12 17:42:25 +0900

    add new files

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var socket;
+
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+    socket = io.connect('https://xadigotest.herokuapp.com/');
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data){
+  noStroke();
+  fill(200,0,100);
+  ellipse(data.x, data.y, 10,10);
+}
+
+function mouseDragged(){
+  console.log('Sending:' + mouseX + ',' + mouseY);
+  var data = {
+   x: mouseX,
+   y: mouseY  
+  }
+  socket.emit('mouse', data); 
+
+  noStroke();
+  fill(200);
+  ellipse(mouseX, mouseY, 10,10);
+}
+
+
+function draw() {
+}
